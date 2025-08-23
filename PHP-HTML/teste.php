70de436c92f7846feacb8de08e6f0fb62021d438
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-08-22 21:03:26 -0300

    Tela de login, estilos, e imagens
    Fiz a tela de login mas ainda esta faltando a imagem e o back-end, css de login feito 100%

---
+++ PHP-HTML/teste.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Aroma Login</title>
+  <link rel="stylesheet" href="../CSS/login.css" />
+</head>
+<body>
+
+  <!-- Formas aleatorias para o fundo -->
+  <div class="shape yellow"></div>
+  <div class="shape pink"></div>
+  <div class="shape small-yellow"></div>
+  <div class="shape small-pink"></div>
+
+  <!-- Container principal -->
+  <div class="container">
+    <!-- Lado esquerdo: formulário -->
+    <div class="login-form">
+      <div class="logo"></div>
+      <h2>Bem-Vindo a nossa biblioteca</h2>
+      <p class="subtitle">Bem vindo de volta. Por favor faça login na sua conta.</p>
+
+      <div class="input-group">
+        <input type="email" placeholder="Email"/>
+      </div>
+      <div class="input-group">
+        <input type="password" placeholder="Senha"/>
+      </div>
+
+      <div class="buttons">
+        <button class="btn btn-login">Login</button>
+      </div>
+
+      <div class="forgot-password">Esqueci minha senha</div>
+
+    </div>
+
+    <!-- Lado direito: imagem e logo -->
+    <div class="image-side">
+        <img class="image" src="gato_feio.jpg" alt="Imagem de um gato feio" />
+    </div>
+  </div>
+
+</body>
+</html>
