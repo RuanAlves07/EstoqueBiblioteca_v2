35fe6b6c8760d43e9b7e9af186fcaa2db05ee733
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-08-22 21:16:22 -0300

    Update login.php

--- PHP-HTML/login.php
+++ PHP-HTML/login.php
@@ -1,3 +1,41 @@
+<?php
+session_start();
+require 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuario WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":email", $email);
+    $stmt->execute();
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($usuario && password_verify($senha, $usuario['senha'])) {
+       //LOGIN BEM SUCEDIDO, DEFINE VARIAVEIS DE SESÃO
+        $_SESSION['usuario'] = $usuario['nome'];
+        $_SESSION['perfil'] = $usuario['id_perfil'];
+        $_SESSION['id_usuario'] = $usuario['id_usuario'];
+
+        // VERIFIVA SE A SENHA É TEMPORARIA
+        if ($usuario['senha_temporaria']) {
+            // Redireciona para a página de troca de senha
+            header("Location: alterar_senha.php");
+            exit();
+        }else{
+            //REDIRECIONA PARA A PAGINA PRINCIPAL   
+        header("Location: principal.php");
+        exit();
+        }
+    }else{
+        //LOGIN INVALIDO
+        echo "<script>alert('E-mail ou senha incorretos!'); window.location.href='login.php';</script>";
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -30,7 +68,7 @@
       </div>
 
       <div class="buttons">
-        <button class="btn btn-login">Login</button>
+        <button class="btn btn-login" href="principal.php">Login</button>
       </div>
 
       <div class="forgot-password">Esqueci minha senha</div>
