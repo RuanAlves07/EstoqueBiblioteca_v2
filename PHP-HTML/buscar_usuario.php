dc13543bcf133e16a426568921523d00d0c36e6c
Isaac <isaac_s_souza@estudante.sesisenai.org.br>
2025-09-02 20:42:33 -0300

    conexão do senai caiu, comitando rapido

--- PHP-HTML/buscar_usuario.php
+++ PHP-HTML/buscar_usuario.php
@@ -1,54 +1,13 @@
 <?php
 session_start();
 require_once 'conexao.php';
+require_once 'Menu.php';
 
 // Verifica se o usuário está logado
 if (!isset($_SESSION['usuario'])) {
     header("Location: login.php");
     exit();
 }
-
-// Obtém o nome do perfil do usuário
-$id_perfil = $_SESSION['perfil'];
-$sqlPerfil = "SELECT nome_perfil FROM perfil WHERE id_perfil = :id_perfil";
-$stmtPerfil = $pdo->prepare($sqlPerfil);
-$stmtPerfil->bindParam(':id_perfil', $id_perfil);
-$stmtPerfil->execute();
-$perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
-$nome_perfil = $perfil['nome_perfil'] ?? 'Perfil Desconhecido';
-
-// Definição das permissões por perfil
-$permissoes = [
-    1 => [
-        "Cadastrar" => ["cadastro_produto.php", "cadastro_perfil.php", "cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_funcionario.php"],
-        "Buscar" => ["buscar_usuario.php", "buscar_perfil.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
-        "Alterar" => ["alterar_usuario.php", "alterar_perfil.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
-        "Excluir" => ["excluir_usuario.php", "excluir_perfil.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],
-        "Emprestimo" => ["emprestimo_de_livros.php"],
-    ],
-    2 => [
-        "Cadastrar" => ["cadastro_cliente.php"],
-        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
-        "Alterar" => ["alterar_cliente.php", "alterar_fornecedor.php"],
-        "Emprestimo" => ["emprestimo.php"],
-    ],
-    3 => [
-        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
-        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
-        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
-        "Excluir" => ["excluir_produto.php"],
-        "Emprestimo" => ["emprestimo.php"],
-    ],
-    4 => [
-        "Cadastrar" => ["cadastro_cliente.php"],
-        "Buscar" => ["buscar_produto.php"],
-        "Alterar" => ["alterar_cliente.php"],
-        "Emprestimo" => ["emprestimo.php"],
-    ],
-];
-
-$opcoes_menu = $permissoes[$id_perfil] ?? [];
-
 // Processar a busca
 $usuarios = [];
 $busca = '';
@@ -87,26 +46,6 @@ $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
-    <!-- Menu de navegação -->
-    <nav>
-        <ul class="menu">
-            <?php foreach($opcoes_menu as $categoria => $arquivos): ?>
-            <li class="dropdown">
-                <a href="#"><?= htmlspecialchars($categoria) ?></a>
-                <ul class="dropdown-menu">
-                    <?php foreach($arquivos as $arquivo): ?>
-                    <li>
-                        <a href="<?= htmlspecialchars($arquivo) ?>">
-                            <?= ucfirst(str_replace(['_', '.php'], [' ', ''], basename($arquivo))) ?>
-                        </a>
-                    </li>
-                    <?php endforeach; ?>
-                </ul>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-    </nav>
 
     <div class="container mt-4">
         <center><h2>Lista de Usuarios</h2></center>
