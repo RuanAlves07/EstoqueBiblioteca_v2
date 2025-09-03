129c22e6c669a87c33a26e327b2a12500accd057
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-09-02 21:25:21 -0300

    ajuste menu novamente

--- PHP-HTML/emprestimo_de_livros.php
+++ PHP-HTML/emprestimo_de_livros.php
@@ -19,10 +19,11 @@ $produtos = $pdo->query("SELECT titulo FROM produto ORDER BY titulo")->fetchAll(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emprestimo de livro</title>
+    <link rel="stylesheet" href="../CSS/styles.css">
 </head>
     <body>
         <center><h2>Emprestimo de livros</h2></center>
-
+        <div class="container">
         <label for="titulo">Selecione o livro:</label>
         <select id="titulo" name="titulo" required>
             <option value="">Selecione um livro...</option>
@@ -32,5 +33,6 @@ $produtos = $pdo->query("SELECT titulo FROM produto ORDER BY titulo")->fetchAll(
                 </option>
             <?php endforeach; ?>
         </select>
+        </div>
     </body>
 </html>
