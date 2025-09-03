be542b256226c2aa52f72f58a473d100e64e64c3
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-09-02 22:00:28 -0300

    update emprestimo de livros

--- PHP-HTML/emprestimo_de_livros.php
+++ PHP-HTML/emprestimo_de_livros.php
@@ -10,7 +10,8 @@ if (!isset($_SESSION['usuario'])) {
 
 
 // Carregar categorias para o select
-$produtos = $pdo->query("SELECT titulo FROM produto ORDER BY titulo")->fetchAll(PDO::FETCH_ASSOC);
+$produtos = $pdo->query("SELECT titulo, quantidade_estoque FROM produto ORDER BY titulo")->fetchAll(PDO::FETCH_ASSOC);
+$categorias = $pdo->query("SELECT id_categoria, nome_categoria FROM categoria ORDER BY nome_categoria")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +23,10 @@ $produtos = $pdo->query("SELECT titulo FROM produto ORDER BY titulo")->fetchAll(
     <link rel="stylesheet" href="../CSS/styles.css">
 </head>
     <body>
+    
+    <form action="emprestimo_de_livros.php" method="POST">
         <center><h2>Emprestimo de livros</h2></center>
+        
         <div class="container">
         <label for="titulo">Selecione o livro:</label>
         <select id="titulo" name="titulo" required>
@@ -31,8 +35,26 @@ $produtos = $pdo->query("SELECT titulo FROM produto ORDER BY titulo")->fetchAll(
                 <option value="<?= $pro['titulo'] ?>">
                     <?= htmlspecialchars($pro['titulo']) ?>
                 </option>
+            </select>
+
+            <label for="quantidade_estoque">Quantidade em Estoque:</label>
+            <input type="number" name="quantidade_estoque" id="quantidade_estoque" 
+                   value="<?= htmlspecialchars($usuario['quantidade_estoque'] ?? '') ?>" readonly>
+
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf" required>
+
+            <label for="senha">Repita sua senha:</label>
+            <input type="text" id="senha" name="senha" required>
+            
             <?php endforeach; ?>
         </select>
         </div>
+
+            <div class="text-center mt-3">
+                <button type="submit" class="btn btn-primary">Confirmar reserva</button>
+                <button type="reset" class="btn btn-danger">Cancelar</button>
+            </div>
+    </form>
     </body>
 </html>
