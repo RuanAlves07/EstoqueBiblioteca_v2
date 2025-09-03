d492a85096b54e44d27f2a2e6c049c7efcdfac87
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-09-02 21:11:49 -0300

    movimentação dashboard.css para pasta correta

--- PHP-HTML/dashboard.php
+++ PHP-HTML/dashboard.php
@@ -90,7 +90,7 @@ $opcoes_menu = $permissoes[$id_perfil];
     <title>Dashboard - Sistema Biblioteca</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="../CSS/dashboard.css">
 </head>
 <body>
     <!-- Sidebar -->
