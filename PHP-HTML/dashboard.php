5e420b891f4759613e666c14fde9b21b1d89740f
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-09-03 22:02:48 -0300

    Add 'seus_emprestimos.php' to Emprestimo permissions
    Updated the Emprestimo permission array to include 'seus_emprestimos.php', allowing users with this permission to access their own loans page.

--- PHP-HTML/dashboard.php
+++ PHP-HTML/dashboard.php
@@ -57,7 +57,7 @@ $permissoes = [
         "Buscar"      => ["buscar_usuario.php", "buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php", "buscar_funcionario.php"],
         "Alterar"     => ["alterar_usuario.php", "alterar_cliente.php", "alterar_fornecedor.php", "alterar_produto.php", "alterar_funcionario.php"],
         "Excluir"     => ["excluir_usuario.php", "excluir_cliente.php", "excluir_fornecedor.php", "excluir_produto.php", "excluir_funcionario.php"],
-        "Emprestimo"  => ["emprestimo_de_livros.php"]
+        "Emprestimo"  => ["emprestimo_de_livros.php","seus_emprestimos.php"]
     ],
     2 => [
         "Cadastrar"   => ["cadastro_cliente.php", "cadastro_fornecedor.php", "cadastro_produto.php"],
