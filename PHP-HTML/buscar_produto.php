8b580753d38588ce5daf70dbe1e439d4b71a3b43
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-09-05 19:20:49 -0300

    Update buscar_produto.php

--- PHP-HTML/buscar_produto.php
+++ PHP-HTML/buscar_produto.php
@@ -122,6 +122,6 @@ $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <!-- Scripts no final -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"></script>
-
+ 
 </body>
 </html>
