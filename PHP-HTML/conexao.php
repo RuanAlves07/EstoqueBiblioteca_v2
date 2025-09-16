fe07b0313ff514c43d4f17924355a95196b28f65
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-09-16 20:04:08 -0300

    ajuste conexão utf8

--- PHP-HTML/conexao.php
+++ PHP-HTML/conexao.php
@@ -4,7 +4,7 @@ $dbname = 'biblioteca_estoquev2';
 $user = 'root';
 $pass = '';
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão com o banco de dados: " . $e->getMessage());
